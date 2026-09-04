e6b5b88c47eb7f7e37478773786c7b1616d5cb08
Divyanshi-shisodia <divyanshishisodia8@gmail.com>
2026-09-04 23:30:14 +0530

    Update SONARSHIELD color theme

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -4,7 +4,7 @@ import TopBar from "./TopBar";
 
 export default function Layout() {
   return (
-    <div className="flex min-h-screen bg-[#F7FCFE]">
+    <div className="flex min-h-screen bg-abyss-900">
       <Sidebar />
       <div className="flex-1 min-w-0">
         <TopBar />
